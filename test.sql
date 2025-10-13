27ee7da2bcee9b2dec04bb6b979bd9dc823c0472
maxatIas <99089139+maxatIas@users.noreply.github.com>
2025-10-13 16:01:03 +0200

    Update utilisateurs+doc test.sql
    ajouté de la documentation et les utilisateurs

--- test.sql
+++ test.sql
@@ -1,3 +1,5 @@
+---/TABLES/---
+--Table utilisateur--
 CREATE TABLE utilisateur(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     Pseudo VARCHAR(40) NOT NULL,
@@ -6,10 +8,8 @@ CREATE TABLE utilisateur(
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 )
-
-INSERT INTO utilisateur('Pseudo','EMAIL','MDP')
-value('tezcat','tezcat.auguste@gmail.com','tezcat')
-
+--FIN Table utilisateur--
+--Table score--
 CREATE TABLE score(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
@@ -19,7 +19,8 @@ CREATE TABLE score(
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 )
-
+--FIN Table score--
+--Table msg--
 CREATE TABLE msg(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     game_id INT UNSIGNED NOT NULL,
@@ -28,14 +29,32 @@ CREATE TABLE msg(
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 )
-
+--FIN Table msg--
+--Table jeu--
 CREATE TABLE jeu (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nom_du_jeu VARCHAR(20),
     PRIMARY KEY(id)
 )
+--FIN Table jeu--
+---/FIN TABLES/---
+---/ELEMENTS DES TABLES/---
+--utilisateurs--
+INSERT INTO utilisateur('Pseudo','EMAIL','MDP')
+value('tezcat','tezcat.auguste@gmail.com','tezcat')
+INSERT INTO utilisateur (Pseudo, EMAIL, MDP)
+VALUES('maxatlas','max@pilato.fr','HDue98è§!98K');
+INSERT INTO utilisateur (Pseudo, EMAIL, MDP)
+VALUES('Nathev','nahtan.blabla@truc.fr','KhbkuhIH!8');
+INSERT INTO utilisateur (Pseudo, EMAIL, MDP)
+VALUES('Thomas','tomas.blublu@truc.fr','krj7§IjIJ8K');
+INSERT INTO utilisateur (Pseudo, EMAIL, MDP)
+VALUES('Random','random.random@truc.fr','sjhfsIHO87§6');
+--FIN utilisateurs--
+--jeu--
 INSERT INTO jeu VALUES(
     1,
     "Power Of Memory"
 )
-
+--FIN jeu--
+---/FIN ELEMENTS DES TABLES/---
