ce7e29a3daf75c732c9b7f575bb94e896bb833cd
Kyllian <tezcat.auguste@gmail.com>
2025-10-14 15:51:25 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -151,7 +151,7 @@ DELETE FROM messagerie_privee
 WHERE id=21
 
 -- Recherche de score par pseudo --
-SELECT nom_du_jeu, username, difficulty, score, created_at
+SELECT nom_du_jeu, user_id, difficulty, score, created_at
 FROM score JOIN utilisateur ON score.user_id = utilisateur.id
 JOIN jeu ON jeu.id = score.game_id
 WHERE user_id LIKE '%%'
