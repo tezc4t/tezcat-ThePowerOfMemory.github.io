19ab905937bdaff59c792eeed8c1116dc529e8c6
maxatIas <max@pilato.fr>
2025-11-06 10:43:24 +0100

    connection

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
+<?php
+    session_start();
+?>
+
 
 <!DOCTYPE html>
 <!--- Define the page as a html web page--->
 <html lang="fr">
 <head>
     <link rel="stylesheet" href="styles.css">
-    <?php include 'partials/head.php'; ?>
+    <?php 
+    include 'partials/head.php';
+    ?>
 </head>
 
 
@@ -19,16 +25,20 @@
                 <h1 id="titremain">Heureux de vous revoir</h1><!---define main title and second title--->
                 <h2 id="titresecond">En esperant que vous vous amuserez !</h2>
             </header>
-            <figcaption id="titre_input" >Email</figcaption> <!---define the forms for connexion--->
-            <input id="text" type="text" placeholder ="Exemple@gmail.com" >
-            <figcaption id="titre_input" >Mot de passe</figcaption>
-            <input id="text" type="text" placeholder="8 caracteres differents" >
-            <a id="mdpo" href="My Account.php">mot de passe oublié?</a><!---redirection for MyAccount.html(renitalisation) page--->
-            <button id="login1">connexion</button>
-            <figcaption id="ou"> ou</figcaption><!--- redirection for google ac connexion--->
-            <a href="https://www.google.com/?hl=fr" id="login2"><img id="google" src="img/google-nouveau-logo-g-removebg-preview (1).png" alt="google"><figcaption id="fig2">se connecter a google</figcaption></a>
-            <figcaption id="pasdecompte"> pas de compte? <a href="inscription.php" id="create">je m'inscris</a></figcaption>
-        
+            <form action="utils/fonctions-connection.php" method="POST">
+                <figcaption id="titre_input" >Email</figcaption> <!---define the forms for connexion--->
+                <input id="text" type="text" name="email" placeholder ="Exemple@gmail.com" >
+                <figcaption id="titre_input" >Mot de passe</figcaption>
+                <input id="text" type="text" name="mdp" placeholder="8 caracteres differents" >
+                <?php 
+                    echo $_SESSION['mvsmdp'];
+                ?>
+                <a id="mdpo" href="My Account.php">mot de passe oublié?</a><!---redirection for MyAccount.html(renitalisation) page--->
+                <button id="login1" type = "submit">connexion</button>
+                <figcaption id="ou"> ou</figcaption><!--- redirection for google ac connexion--->
+                <a href="https://www.google.com/?hl=fr" id="login2"><img id="google" src="img/google-nouveau-logo-g-removebg-preview (1).png" alt="google"><figcaption id="fig2">se connecter a google</figcaption></a>
+                <figcaption id="pasdecompte"> pas de compte? <a href="inscription.php" id="create">je m'inscris</a></figcaption>
+            </form>
 
         </div><!---img on the right--->
         <div class="image" >
---
+++ utils/fonctions-connection.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    require 'database.php';
+    $pdo = getPDO();
+    $email = $_POST["email"];
+    $mdp = $_POST["mdp"];
+    $email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+    $mdp = htmlspecialchars($mdp, ENT_QUOTES, 'UTF-8');
+    $stmt = $pdo->query("SELECT id FROM utilisateur WHERE email = '$email' AND mdp = '$mdp'");
+    $u = $stmt -> fetch();
+    if ($u != NULL){
+        header('Location: ../accueil.php');
+        $_SESSION['mvsmdp'] = '';
+        $userid = $u['id'];
+    } else {
+        $_SESSION['mvsmdp'] = 'adresse email ou mot de passe incorrect';
+        header('Location: /index.php');
+    };
