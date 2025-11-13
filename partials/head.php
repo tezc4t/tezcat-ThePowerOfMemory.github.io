d6923ab0045191c884c64653cc0013b1c3636dde
maxatIas <max@pilato.fr>
2025-11-13 16:14:59 +0100

    small change

--- partials/head.php
+++ partials/head.php
@@ -1,4 +1,7 @@
 <!-- head.php -->
+ <?php    
+    session_start();
+ ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,4 @@
 <?php
-    session_start();
     require 'utils/fonctions-header.php';
 ?>
 <header>
